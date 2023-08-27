07058f5df5b5d462abde9a63e8cab0ba62dad431
PhuNH <1079742+PhuNH@users.noreply.github.com>
2023-08-27 17:02:41 +0200

    Support compilation
    The PO folder must contain one sub-folder for each language, each sub-folder contains PO files in that language.

--- hugo_gettext/cli.py
+++ hugo_gettext/cli.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser, RawTextHelpFormatter
 
 from .extraction import extract
 from .generation import generate
+from .compilation import compile_po
 
 
 def main():
@@ -26,6 +27,12 @@ def main():
                                          formatter_class=RawTextHelpFormatter)
     generate_cmd.add_argument('-k', '--keep-locale', action='store_true', help='do not delete locale folder')
     generate_cmd.set_defaults(func=generate)
+
+    compile_po_cmd = subparsers.add_parser('compile', help='compile translated messages to binary format',
+                                           formatter_class=RawTextHelpFormatter)
+    compile_po_cmd.add_argument('dir', help='path of the directory containing subdirectories with PO files inside\n'
+                                            'in the form of {dir}/{lang}/*.po')
+    compile_po_cmd.set_defaults(func=compile_po)
 
     args = parser.parse_args()
     level = logging.WARNING if args.quiet else logging.INFO
---
+++ hugo_gettext/compilation.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2023 Phu Hung Nguyen <phuhnguyen@outlook.com>
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import logging
+import os
+import subprocess
+
+
+def compile_po(args):
+    """
+    Compile translated messages to binary format stored in 'locale/{lang}/LC_MESSAGES' directory
+    :param args: arguments passed in command line, containing
+        - dir: path of the directory containing subdirectories with PO files inside, in the form of {lang}/*.po
+    :return: None
+    """
+    po_dir = args.dir
+    for lang in os.listdir(po_dir):
+        target_path = f'locale/{lang}/LC_MESSAGES'
+        os.makedirs(target_path, exist_ok=True)
+        src_path = f'{po_dir}/{lang}'
+
+        for po in os.listdir(src_path):
+            po_path = f'{src_path}/{po}'
+            mo_path = f'{target_path}/{po[:-2]}mo'
+            command = f'msgfmt {po_path} -o {mo_path}'
+            subprocess.run(command, shell=True, check=True)
+        logging.info(f'Compiled {lang}')
